6be2b591356c67e3275fdaf61f5ea192e4eb6dac
Ekaterina Kozhina <katerinabonia@mail.ru>
2021-11-12 13:54:27 +0300

    Add check_borders

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+bool bulb0 = false;
+
+bool border(double now, bool limit) {
+    if (now == 0) {
+        if (!bulb0) {
+            bulb0 = true;
+            limit = true;
+        } else {
+            limit = false;
+        }
+    } else if (now < 0) {
+        limit = false;
+    } else {
+        limit = true;
+    }
+    return limit;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
