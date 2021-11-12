b98e057641937dc67f4e7f71b3411efee66a4d1d
Ekaterina Kozhina <katerinabonia@mail.ru>
2021-11-12 14:18:50 +0300

    Add structure_queue

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("queue.in", "r", stdin);
+    freopen("queue.out", "w", stdout);
+    int n, pointer_in = 0, pointer_out = 0;
+    char sign, value[15];
+    cin >> n;
+    char arr[n][15];
+    for (int i = 0; i < n; i++) {
+        cin >> sign;
+        if (sign == '+') {
+            cin >> value;
+            strcpy(arr[pointer_in], value);
+            pointer_in++;
+        } else {
+            cout << arr[pointer_out] << '\n';
+            pointer_out++;
+        }
+    }
     return 0;
 }
