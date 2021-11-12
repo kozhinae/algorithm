14c49a2f3b1efde3fc68880f23e1fe4678421a7e
Ekaterina Kozhina <katerinabonia@mail.ru>
2021-11-12 13:30:11 +0300

    Add input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ifstream file("brackets.in");
+    freopen("brackets.out", "w", stdout);
+    while (file) {
+        int pointer = 0, flag = 1;
+        string line;
+        char arr[10000];
+        file >> line;
+    }
     return 0;
 }
