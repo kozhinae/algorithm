cb0457ac9c465f2625a567756b65434874637480
Ekaterina Kozhina <katerinabonia@mail.ru>
2021-11-12 14:14:50 +0300

    Add input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("queue.in", "r", stdin);
+    freopen("queue.out", "w", stdout);
+    int n, pointer_in = 0, pointer_out = 0;
+    char sign, value[15];
+    cin >> n;
+    char arr[n][15];
     return 0;
 }
